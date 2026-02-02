21a23d71912eaaba825e767122f01d73eabf7322
arifbk-bmk <arifrkilledar@gmail.com>
2026-02-02 05:05:10 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+//datatypes
+ let age="53"  /// number datatype
+// bolean datatype
+let arif=true
+//string datatype
+let name1="arif"
+//null datatype
+let nullvalue=null
+console.log([age,name1,nullvalue,arif])
+//symbol datatype
+//undefined datatype
+ ////these are all primitive datatypes
+            
