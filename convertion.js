e3c4645066bc4c2ababca9dd814b5555b178486c
arifbk-bmk <arifrkilledar@gmail.com>
2026-02-04 14:39:32 +0000

    datasummary

--- convertion.js
+++ convertion.js
@@ -1,6 +1,21 @@
-//converition data 
-// ek data kooo dusre dataype me badalnekea 
-let score="33abc"
-console.log(typeof(score))
-let convert=Number(score)
-console.log(convert)
+// //converition data 
+// // ek data kooo dusre dataype me badalnekea 
+// let score="33abc"
+// console.log(typeof(score))
+// let convert=Number(score)
+// console.log(convert)
+
+
+// camparistion  topic 
+// console.log("2"<1);
+// console.log("2">1);
+// console.log(null< 0);
+// console.log(null==0)
+// note remeber this all shit
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
+
---
+++ datasmy.js
@@ -0,0 +1,31 @@
+// datatypes summuary
+// there are 2 major types of datatypes
+// primitive----nonprimitive
+
+
+// 1)primitive
+/*
+string
+number
+bollen
+null---empty
+undefined
+symbol -- kisi bhi vallue ko unique banake liye
+bigint big sintific values
+
+
+
+
+
+// nonprimitive datatype(refernce)
+
+Array 
+objects
+funtion
+
+
+
+*/
+const id = Symbol('123')
+const idk = Symbol('123')
+console.log(id===idk)
