d5bac37ee5b054cf59cc17d6fb9c7004ee9a58be
arifbk-bmk <arifrkilledar@gmail.com>
2026-02-05 15:41:26 +0000

    stackkaye

---
+++ stack.js
@@ -0,0 +1,24 @@
+// stack and heap 
+
+
+     //major thing in memmory staorege is 
+
+
+     //all primitive data store in the (stack)
+
+
+
+     //all non primitive data type (heap memmory)
+
+
+     let arif={
+       hight :5.10,
+        age:10
+     }
+    //  console.log()
+      let arif2=arif
+       arif2.hight=5.3
+      
+    
+    console.log(arif)
+    
