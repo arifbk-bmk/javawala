07b3de98146f2a5f5438ebe8d51591104fb7eaa1
arifbk-bmk <arifrkilledar@gmail.com>
2026-02-06 16:17:11 +0000

    stringj

--- string.js
+++ string.js
@@ -1,4 +1,17 @@
-// string ka new synatax hai dek
-let name1 = "arif"
-let Proffetion="software_enginear"
-console.log(` my name is ${name1} and my proffetion is ${Proffetion}`)
+// // string ka new synatax hai dek
+// let name1 = "arif"
+// let Proffetion="software_enginear"
+// console.log(` my name is ${name1} and my proffetion is ${Proffetion}`)
+ let gameName= "arid"
+// console.log(gameName[0])
+// console.log(gameName.toUpperCase());
+// console.log(gameName.indexOf("r"))
+
+// console.log(gameName.replace("a","9"))
+// console.log(gameName.includes("h"))
+// console.log(gameName.substring(0,3))
+// console.log(gameName.trim())
+const str="arif will became a millinear";
+const words1=str.split('');
+console.log(words1 [2]);
+
