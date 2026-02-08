4a0343a3a680de117417b41d2ab3f1e6bb9d0d79
arifbk-bmk <arifrkilledar@gmail.com>
2026-02-08 17:15:02 +0000

    numand math

---
+++ num.js
@@ -0,0 +1,10 @@
+// // const score = 885
+// // console.log(score)
+// const balence = new Number(2234.534)
+// console.log(balence.toPrecision(3))
+const arif=100000
+console.log(arif.toLocaleString('en-In'))
+// most reading forrmuls
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1)+min))
