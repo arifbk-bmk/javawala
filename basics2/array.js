a5dae839cd124118334b9e238ce9bb7d88dd51d8
arifbk-bmk <arifrkilledar@gmail.com>
2026-02-10 15:46:48 +0000

    arrayks ye

---
+++ basics2/array.js
@@ -0,0 +1,19 @@
+// data stractures 
+// arrays
+
+
+// arrays methods
+const arr1=[2,3,4,5,2]
+arr1.splice(1,2,9,8)
+console.log(arr1)
+// important methods to rember at any cost 
+// 1)    slice
+// 2)    splice 
+// 3)    unshipt
+// 4)    shift
+ 
+
+
+
+
+
