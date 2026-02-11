efa056d757a92832f7dff930983f7f47abb7c0fe
arifbk-bmk <arifrkilledar@gmail.com>
2026-02-11 15:58:31 +0000

    arraypart2

--- basics2/array.js
+++ basics2/array.js
@@ -1,19 +1,36 @@
-// data stractures 
-// arrays
+// // data stractures 
+// // arrays
 
 
-// arrays methods
-const arr1=[2,3,4,5,2]
-arr1.splice(1,2,9,8)
-console.log(arr1)
-// important methods to rember at any cost 
-// 1)    slice
-// 2)    splice 
-// 3)    unshipt
-// 4)    shift
+// // arrays methods
+// const arr1=[2,3,4,5,2]
+// arr1.splice(1,2,9,8)
+// console.log(arr1)
+// // important methods to rember at any cost 
+// // 1)    slice
+// // 2)    splice 
+// // 3)    unshipt
+// // 4)    shift
  
+// array part2
+// const arif=["ff","gg"]
+// const beef=["hh","jk"]
+// // arif.push(beef)
+// const zz=[...arif,...beef]
+// console.log(zz)
+
+
+// const af=[1,2,3,[4,5,6,[7,8,9]]]
+// const gg=af.flat(2)
+// console.log(gg)
+
+
+//const gg=("sssrif")
+//console.log(Array.from{name1:"arif"} )//interesting
 
 
 
-
-
+// important array to remember 
+//1)isarray
+//2)from
+//3)Of
